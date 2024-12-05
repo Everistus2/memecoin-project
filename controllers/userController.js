039663cb2f6656d2b1c2773b9babf28d6778882e
fzgram <fzgram8@gmail.com>
2024-12-05 20:11:49 +0100

    Before trading functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -98,48 +98,7 @@ const withdraw = async (req, res) => {
 
 //req.marketAddress, req.userId, req.walletAddress, req.side, req.price, req.size
 const trade = async (req, res) => {
-  try {
-    const marketAddress = req.marketAddress;
-    const serumProgramId = new PublicKey("9xQeWvG816bUx9EPvT5MCfbw4zgKuKisEUK16tFfCQpP");
-
-    const user = await walletModel.findOne({ userId: req.userId, address: req.walletAddress });
-    const traderKeypair = Keypair.fromSecretKey(Uint8Array.from([user.privateKey]));
-
-    const loadMarket = async () => {
-      return await Market.load(connection, marketAddress, {}, serumProgramId);
-    };
-
-    const placeOrder = async (side, price, size) => {
-      try {
-        const market = await loadMarket();
-
-        const orderParams = {
-          owner: traderKeypair,
-          payer: traderKeypair.publicKey, // Token account for USDC or SOL
-          side, // "buy" or "sell"
-          price, // Price per unit
-          size, // Number of units
-          orderType: "limit", // or 'market'
-          clientId: new Date().getTime(), // Unique order identifier
-        };
-
-        const transaction = await market.makePlaceOrderTransaction(connection, orderParams);
-        const signature = await connection.sendTransaction(transaction, [traderKeypair]);
-
-        console.log("Order placed! Transaction signature:", signature);
-        res.json(200).send({
-          success: true,
-          message: "trade performed successfully",
-        });
-        return signature;
-      } catch (err) {
-        console.error("Error placing order:", err);
-      }
-    };
-    placeOrder(req.side, req.price, req.size);
-  } catch (err) {
-    console.log("Error during trading:", err);
-  }
+      
 };
 
 const test = async (req, res) => {
